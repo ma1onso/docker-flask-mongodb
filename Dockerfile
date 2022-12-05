add3fcc93ed62eec44e185cb04f0bb632fae6a43
Alonso <alons45@gmail.com>
2022-12-04 20:59:40 -0500

    Implement connection with mongodb

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
-FROM mongo:5.0.14
+FROM python:3.11.0-bullseye
 WORKDIR /app
 COPY . /app
-RUN apt update -y
-RUN apt install python3.8 python3-pip -y
 RUN pip install -r requirements.txt
 
 CMD ["flask", "--app", "src/app", "--debug", "run", "--host=0.0.0.0"]
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,15 @@
+import pymongo
+
+from constants import COMMAND_NOT_STARTED_STATUS
+
+
 def insert_to_mongodb(command, status):
-    print('Save value to MongoDB')
+    client = pymongo.MongoClient(
+        "mongodb+srv://malonso:PASSWORD@cluster0.aba4cnn.mongodb.net/?retryWrites=true&w=majority"
+    )
+    
+    db = client.commands
+    
+    return db.cmd_collection.insert_one({
+        'command': 'ps', 'status': COMMAND_NOT_STARTED_STATUS
+    }).inserted_id
