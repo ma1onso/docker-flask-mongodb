0e40e787142017d19e8d5afade808c173ba7957d
Alonso <alons45@gmail.com>
2022-12-04 21:20:37 -0500

    prepare the code to implement the async task

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 
-from utils import insert_to_mongodb
+from utils import MongoClient
 from constants import COMMAND_NOT_STARTED_STATUS
 
 app = Flask(__name__)
@@ -9,6 +9,8 @@ app = Flask(__name__)
 @app.route('/new_task', methods=['POST'])
 def new_task():
     command = request.form.get('command')
-    id = insert_to_mongodb(command=command, status=COMMAND_NOT_STARTED_STATUS)
+
+    mongo_client = MongoClient()
+    id = mongo_client.insert_command(command=command, status=COMMAND_NOT_STARTED_STATUS)
 
     return {'id': id}
--- src/utils.py
+++ src/utils.py
@@ -3,14 +3,19 @@ import pymongo
 from constants import COMMAND_NOT_STARTED_STATUS
 
 
-def insert_to_mongodb(command, status):
-    client = pymongo.MongoClient(
-        # TODO: Set a env variable for this password
-        "mongodb+srv://malonso:PASSWORD@cluster0.aba4cnn.mongodb.net/?retryWrites=true&w=majority"
-    )
-    
-    db = client.commands
-    
-    return db.cmd_collection.insert_one({
-        'command': 'ps', 'status': COMMAND_NOT_STARTED_STATUS
-    }).inserted_id.__str__()
+class MongoClient:
+    def __init__(self):
+        self.client = pymongo.MongoClient(
+            # TODO: Set a env variable for this password
+            "mongodb+srv://malonso:PASSWORD@cluster0.aba4cnn.mongodb.net/?retryWrites=true&w=majority"
+        )
+        self.db = self.client.commands
+
+    def insert_command(self, command, status):
+        return self.db.cmd_collection.insert_one({
+            'command': 'ps', 'status': COMMAND_NOT_STARTED_STATUS
+        }).inserted_id.__str__()
+
+    def find_not_execute_commands(self):
+        # TODO: Set a lightweight alternative to Celery to read the MongoDB and run the commands
+        return self.db.cmd_collection.find({'status': COMMAND_NOT_STARTED_STATUS})
